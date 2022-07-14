a9428ea1f2d3c303ccc56ae46419c1dfc3778b85
marcviny <103757801+marcviny@users.noreply.github.com>
2022-07-14 03:26:35 -0300

    Update clinica.sql
    botei a parte das consultas

--- clinica.sql
+++ clinica.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14-Jul-2022 às 03:08
+-- Tempo de geração: 14-Jul-2022 às 08:25
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -52,6 +52,13 @@ CREATE TABLE `animal` (
   `id_animal` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `animal`
+--
+
+INSERT INTO `animal` (`nome_animal`, `id_cliente`, `id_animal`) VALUES
+('ANIMAL1', 101, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -74,7 +81,24 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`nome_cliente`, `id_cliente`, `email_cliente`, `cpf_cliente`, `celular_cliente`, `data_cadastro_cliente`, `rua_cliente`, `bairro_cliente`) VALUES
-('TESTE2 TESTANDO', 101, 'teste2@testando.com', '002', '148814814148', '2022-07-13', 'rua do test 2', 0);
+('TESTE2 TESTANDO', 101, 'teste2@testando.com', '002', '148814814148', '2022-07-13', 'rua do test 2', 0),
+('TEST3', 102, 'teste3@testando.com', '003', '333333', '2022-07-14', 'rua3', 0),
+('TES4', 103, 'tes4@tes4.com', '4', '4444', '2022-07-14', 'r4', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `consultas`
+--
+
+CREATE TABLE `consultas` (
+  `id_consulta` int(4) NOT NULL,
+  `id_animal` int(4) NOT NULL,
+  `id_cliente` int(4) NOT NULL,
+  `data` varchar(10) NOT NULL,
+  `hora` int(2) NOT NULL,
+  `observacoes` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Índices para tabelas despejadas
@@ -100,6 +124,14 @@ ALTER TABLE `cliente`
   ADD PRIMARY KEY (`id_cliente`);
 
 --
+-- Índices para tabela `consultas`
+--
+ALTER TABLE `consultas`
+  ADD PRIMARY KEY (`id_consulta`),
+  ADD KEY `fk_consulta_animal` (`id_animal`),
+  ADD KEY `fk_consulta_cliente` (`id_cliente`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -113,7 +145,13 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `id_cliente` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+  MODIFY `id_cliente` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
+
+--
+-- AUTO_INCREMENT de tabela `consultas`
+--
+ALTER TABLE `consultas`
+  MODIFY `id_consulta` int(4) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restrições para despejos de tabelas
@@ -124,6 +162,13 @@ ALTER TABLE `cliente`
 --
 ALTER TABLE `animal`
   ADD CONSTRAINT `fk_animal_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
+
+--
+-- Limitadores para a tabela `consultas`
+--
+ALTER TABLE `consultas`
+  ADD CONSTRAINT `fk_consulta_animal` FOREIGN KEY (`id_animal`) REFERENCES `animal` (`id_animal`),
+  ADD CONSTRAINT `fk_consulta_cliente` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id_cliente`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
